4ce1b3cdce9d0857d322c909f11ea8f1aff71705
Jean Pascal Cyusa Shyaka <shyakaadolf@gmail.com>
2026-03-19 09:30:23 -0400

    Change profile image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -30,7 +30,7 @@ export default function Hero() {
                         <div className="relative w-full h-full overflow-hidden shadow-2xl flex items-center justify-center">
                             <div className="relative w-[85%] h-[85%]">
                                 <Image
-                                    src="/images/profile.webp"
+                                    src="/images/profile.jpeg"
                                     alt="Jean Pascal Cyusa Shyaka"
                                     fill
                                     className="object-cover"
